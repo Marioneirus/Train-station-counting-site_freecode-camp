f36044fa697bdba35cff8a2ba1168b0c0e26fff0
marioneirus <chiemelieudoagwa@gmail.com>
2022-11-15 17:38:19 +0100

    added more button with html and more functions with javascript

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let resetEl = document.getElementById("reset-el")
+
 let count = 0;
 
-let countEl = document.getElementById("count-el")
-console.log(countEl)
+
+console.log(countEl);
 
 function increment() {
-    count = count + 1;
-    countEl.innerHTML = count
+    count += 1;
+    countEl.innerHTML = count;
 }
 
 function save() {
-    console.log(count)
+    let save = count + " - ";
+    
+    saveEl.textContent += save;
+    countEl.innerHTML = 0;
+    count = 0;
 }
+
+function reset() {
+    countEl.textContent = 0;
+    saveEl.textContent = " ";
+    count = 0;
+}
+
+function decrease() {
+    countEl.textContent -= 1;
+}
+   
+
